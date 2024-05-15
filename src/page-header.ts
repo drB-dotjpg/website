aaed356c860d397531058c83b0b43c5bc9265185
Derek <derek.bond24@gmail.com>
2024-05-15 18:49:37 -0500

    may 2024 updates

--- src/page-header.ts
+++ src/page-header.ts
@@ -71,18 +71,20 @@ export class PageHeaderElement extends LitElement {
             <link rel="stylesheet" href="/styles.css" />
             <div class="name font-syne">Derek Bond</div>
                 <div class="bigger font-space">
-                    <text-changing-span class="font-mono box less-padding" .texts=${["Management Info Systems Student", "Club Leader", "Student IT Worker"]}></text-changing-span>
+                    <text-changing-span class="font-mono box less-padding" .texts=${["Management Info Systems Student", "Freelance Software Developer"]}></text-changing-span>
                     <strong>&nbsp;by day</strong>
                 </div>
                 <div class="bigger font-space">
-                    <text-changing-span class="font-mono box less-padding" .texts=${["Developer", "Esports Broadcast Producer", "UX designer"]} .delay=${.5}></text-changing-span>
+                    <text-changing-span class="font-mono box less-padding" .texts=${["Esports Broadcast Producer", "UX designer"]} .delay=${.5}></text-changing-span>
                     <strong>&nbsp;by night</strong>
                 </div>
 
             <p class="font-space box">
-                Passionate and detail-oriented student at Iowa State University. Seeking post-graduation employment in the application development or the information technology field. Experienced in problem solving within team based environments both in work and in my hobbies.
+                Passionate and detail-oriented student at Iowa State University. Seeking post-graduation employment as a software developer. Experienced in problem solving within team based environments both in work and in my hobbies.
                 <br>
                 <strong>Graduating with a Bachelor of Science in Management Information Systems on August 2, 2024.</strong>
+                <br><br>
+                This website was made by me to showcase my skills and passion.
             </p>
 
             <a class="learn-more" id="learn-more" @click=${this.learnMoreClicked}>Learn more about me</a>
