6be41992ba15e26dddfe7a1becafdecfbe9a7423
Derek <derek.bond24@gmail.com>
2025-05-01 10:30:13 -0500

    job update

--- src/App.vue
+++ src/App.vue
@@ -20,9 +20,8 @@ watch(router.currentRoute, () => {
         <header
             class="xl:w-1/3 flex flex-col xl:items-end gap-3 xl:text-right -m-3 p-3 md:-m-6 md:p-6 bg-black/30 backdrop-blur-sm xl:m-0 xl:p-0 xl:bg-transparent xl:backdrop-blur-none">
             <h1 class="font-bold text-4xl xl:text-6xl">Derek Bond</h1>
-            <p class="text-pretty text-sm xl:text-base">Freelance Software Engineer & Management Information Systems
-                Graduate seeking full time programming
-                opportunities.</p>
+            <p class="text-pretty text-sm xl:text-base">Software Consultant, Management Information Systems
+                Graduate, & Freelance Esports Software Developer.</p>
             <nav class="flex gap-2 flex-wrap xl:justify-end">
                 <RouterLink to="/"
                     class="no-underline py-1.5 px-3 bg-black/50 rounded text-lg hover:shadow-lg hover:bg-white hover:text-black transition-colors backdrop-blur-sm"
