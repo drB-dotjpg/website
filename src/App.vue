b050f4bec82f2e249ef984c1c2471d63e9b95d64
Derek <derek.bond24@gmail.com>
2026-06-13 13:45:11 -0500

    restart

--- src/App.vue
+++ src/App.vue
@@ -1,70 +1,29 @@
-<script setup lang="ts">
-import { RouterLink, RouterView, useRouter } from 'vue-router';
-import "./components/anim-bg";
-import { ref, watch } from 'vue';
-
-const router = useRouter();
-const content = ref<HTMLElement | null>(null);
-
-watch(router.currentRoute, () => {
-    window.scrollTo({ top: 0 });
-    if (content.value) {
-        content.value.scrollTo({ top: 0 });
-    }
-});
-</script>
-
 <template>
-    <div
-        class="text-[#F0FAF6] p-3 md:p-6 xl:pr-0 flex gap-6 xl:gap-16 flex-col xl:w-dvw xl:h-dvh xl:flex-row xl:items-center">
+    <div class="flex min-h-dvh gap-x-full bg-dark text-light">
         <header
-            class="xl:w-1/3 flex flex-col xl:items-end gap-3 xl:text-right -m-3 p-3 md:-m-6 md:p-6 bg-black/30 backdrop-blur-sm xl:m-0 xl:p-0 xl:bg-transparent xl:backdrop-blur-none">
-            <h1 class="font-bold text-4xl xl:text-6xl">Derek Bond</h1>
-            <p class="text-pretty text-sm xl:text-base">Software Consultant, Management Information Systems
-                Graduate, & Freelance Esports Software Developer.</p>
-            <nav class="flex gap-2 flex-wrap xl:justify-end">
-                <RouterLink to="/"
-                    class="no-underline py-1.5 px-3 bg-black/50 rounded text-lg hover:shadow-lg hover:bg-white hover:text-black transition-colors backdrop-blur-sm"
-                    :class="{
-                        'bg-emerald-600/50': router.currentRoute.value.path === '/'
-                    }">
-                    About</RouterLink>
-                <RouterLink to="/skills"
-                    class="no-underline py-1.5 px-3 bg-black/50 rounded text-lg hover:shadow-lg hover:bg-white hover:text-black transition-colors backdrop-blur-sm"
-                    :class="{
-                        'bg-emerald-600/50': router.currentRoute.value.path.includes('/skills')
-                    }">
-                    Skills</RouterLink>
-                <RouterLink to="/experience"
-                    class="no-underline py-1.5 px-3 bg-black/50 rounded text-lg hover:shadow-lg hover:bg-white hover:text-black transition-colors backdrop-blur-sm"
-                    :class="{
-                        'bg-emerald-600/50': router.currentRoute.value.path.includes('/experience')
-                    }">
-                    Experience</RouterLink>
-                <RouterLink to="/projects"
-                    class="no-underline py-1.5 px-3 bg-black/50 rounded text-lg hover:shadow-lg hover:bg-white hover:text-black transition-colors backdrop-blur-sm"
-                    :class="{
-                        'bg-emerald-600/50': router.currentRoute.value.path.includes('/projects')
-                    }">
-                    Projects</RouterLink>
-            </nav>
-            <nav class="flex gap-2 flex-wrap xl:justify-end">
-                <a href="https://www.linkedin.com/in/derek-bond-759b301b5/">LinkedIn</a>
-                <a href="https://github.com/drB-dotjpg">Github</a>
-            </nav>
+            class="sticky top-0 flex h-screen w-1/3 flex-col justify-center p-full pr-0 text-6xl font-bold"
+        >
+            <h1>Derek Bond</h1>
         </header>
 
-        <div class="xl:w-2/3 xl:h-dvh xl:overflow-y-auto relative" ref="content">
-            <router-view v-slot="{ Component }">
-                <transition>
-                    <component :is="Component"
-                        class="absolute xl:pr-6 py-6 flex flex-col min-h-full w-full justify-center" />
-                </transition>
-            </router-view>
+        <div class="flex w-2/3 flex-col p-full pt-0 pl-0">
+            <div class="sticky top-0 z-10 w-full">
+                <div class="h-gap-full w-full bg-dark"></div>
+                <nav class="h-16 w-full border bg-dark"></nav>
+                <div class="h-gap-full w-full bg-linear-to-t from-transparent to-dark"></div>
+            </div>
+
+            <main class="flex flex-col">
+                <template v-for="i in 40" :key="i">
+                    <div
+                        class="w-full border p-full text-lg font-bold transition-[border-radius] duration-100 ease-in-out not-first:-mt-px hover:rounded-on"
+                    >
+                        {{ i }} lorem ipsum sit amet
+                    </div>
+                </template>
+            </main>
         </div>
     </div>
-
-    <div class="fixed top-0 left-0 w-screen h-screen -z-10 bg-[#232F2A] pointer-events-none">
-        <anim-bg></anim-bg>
-    </div>
 </template>
+
+<script setup lang="ts"></script>
--- src/types/experience.ts
+++
@@ -1,8 +0,0 @@
-export type Experience = {
-    name: string;
-    position: string;
-    tags: string[];
-    preview: string;
-    body: string;
-    img?: string[];
-};
